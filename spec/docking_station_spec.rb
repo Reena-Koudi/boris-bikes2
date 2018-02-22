5d8ab083d563ae18eebb479f4edefaef925d27f6
Reena Koudi <reenakoudi@Reenas-MacBook-Pro.local>
2018-02-22 17:21:30 +0000

    Added dock bike and return bike methods along with error handling message for no bikes available

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,26 @@
 require 'docking_station'
+require 'bike'
 describe DockingStation do
-  describe '.release_bike' do
-    it "release bike on docking station" do
-      expect(DockingStation).to respond_to '.release_bike'
-
-    end
-
-  end
+ describe '#release_bike' do
+    it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
 
 end
+end
+
+  it 'docks something' do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+#  it {is_expected.to respond_to(:bike)}
+
+  it 'returns docked bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
+
+end
+#it { is_expected.to respond_to(:zero?).with(0).arguments }
