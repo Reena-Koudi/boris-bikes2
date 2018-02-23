3dad6b05399f870dc0ef75b45af16a318eaaa873
Reena Koudi <reenakoudi@Reenas-MacBook-Pro.local>
2018-02-23 15:04:33 +0000

    Adding an array of bikes and refactoring of code

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,15 +1,36 @@
+require './lib/bike.rb'
 class DockingStation
+  DEFAULT_CAPACITY = 20
   attr_reader :bike
+ def initialize
+   @bikes = []
+ end
 
-  def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+def release_bike
+    if empty?
+       fail 'No bikes available'
+    else
+       @bikes.pop
+    end
   end
 
-  def dock(bike)
-    @bike = bike
+def dock(bike)
+  if full?
+    fail 'Docking station full'
+     else
+
+      @bikes.push(bike)
+    bike
   end
+end
+private
+def full?
+  @bikes.length == DEFAULT_CAPACITY
 
-  
+end
 
+
+  def empty?
+    @bikes.length == 0
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,7 @@
 require 'docking_station'
 require 'bike'
 describe DockingStation do
+  DEFAULT_CAPACITY = 20
  describe '#release_bike' do
     it 'raises an error when there are no bikes available' do
       expect { subject.release_bike }.to raise_error 'No bikes available'
@@ -8,17 +9,21 @@ describe DockingStation do
 end
 end
 
-  it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+describe '#dock' do
+  it 'raises an error while the capacity is full' do
+    DEFAULT_CAPACITY.times{ subject.dock(Bike.new) }
+    expect { subject.dock Bike.new}.to raise_error 'Docking station full'
+
   end
+ end
+
 
 #  it {is_expected.to respond_to(:bike)}
 
   it 'returns docked bikes' do
     bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+    #subject.dock(bike)
+    expect(subject.dock(bike)).to eq bike
   end
 
 
